53ae9d39f6d03f88feaf631695937dfdc5a57534
Skye Viau <skye.viau@gmail.com>
2019-01-03 07:55:02 -0500

    Menu (with clickable buttons wow)

--- sm.py
+++ sm.py
@@ -46,18 +46,66 @@ def set_activity(songName):
     return activity
 
 def mainMenu():
+    # Discord RPC
     client.update_activity(menuActivity)
+
+    # Load sound files and play menu music
     pygame.mixer.music.load("assets/menu.ogg")
+    clickSound = pygame.mixer.Sound("assets/click.ogg")
     pygame.mixer.music.play(-1)
-    menuBackgroundColor = (107, 107, 107)
+
+    # Define main menu only colors
+    menuBackgroundColor = (52, 52, 52)
+    menuTextColor = (240, 240, 240)
+
+    # PyMania game logo on menu
     gameTitleMenu = pygame.transform.smoothscale(gameTitle, (552, 276))
     titleRect = gameTitleMenu.get_rect()
     titleRect.centerx = windowSurface.get_rect().centerx
-    titleRect.centery = windowSurface.get_rect().centery - 260
+    titleRect.centery = windowSurface.get_rect().centery - 150
+
+    # Buttons
+    buttonFont = pygame.font.Font('assets/mainMenu.ttf', 45)
+
+    start = buttonFont.render('Start Game', True, menuTextColor)
+    startRect = start.get_rect()
+    startRect.centerx = windowSurface.get_rect().centerx
+    startRect.centery = windowSurface.get_rect().centery + 120
+
+    quit = buttonFont.render('Quit', True, menuTextColor)
+    quitRect = quit.get_rect()
+    quitRect.centerx = windowSurface.get_rect().centerx
+    quitRect.centery = windowSurface.get_rect().centery + 190
+
+    # Draw everything to window
     windowSurface.fill(menuBackgroundColor)
     windowSurface.blit(gameTitleMenu, titleRect)
-    pygame.display.update()
+    windowSurface.blit(start, startRect)
+    windowSurface.blit(quit, quitRect)
 
+    # Make the menu I N T E R A C T I V E
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        print(mouse)
+        print(click)
+        pygame.display.update()
+        if startRect.x+start.get_width() > mouse[0] > startRect.x and startRect.y+start.get_height() > mouse[1] > startRect.y:
+            if click[0] == 1:
+                clickSound.set_volume(0.6)
+                clickSound.play()
+
+        if quitRect.x+quit.get_width() > mouse[0] > quitRect.x and quitRect.y+quit.get_height() > mouse[1] > quitRect.y:
+            if click[0] == 1:
+                clickSound.set_volume(0.6)
+                clickSound.play()
+                pygame.quit()
+                sys.exit()
 
 def testLoop():
     while True:
